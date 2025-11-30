8a0b5ee169360b802108ccdd7c25f9548c8a6465
Aster-00 <ammarabdelkhalek021@gmail.com>
2025-11-30 12:03:05 +0200

    models to Models

--- payroll-tracking.module.ts
+++ payroll-tracking.module.ts
@@ -2,9 +2,9 @@ import { forwardRef, Module } from '@nestjs/common';
 import { PayrollTrackingController } from './payroll-tracking.controller';
 import { PayrollTrackingService } from './payroll-tracking.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { refunds, refundsSchema } from './models/refunds.schema';
-import { claims, claimsSchema } from './models/claims.schema';
-import { disputes, disputesSchema } from './models/disputes.schema';
+import { refunds, refundsSchema } from './Models/refunds.schema';
+import { claims, claimsSchema } from './Models/claims.schema';
+import { disputes, disputesSchema } from './Models/disputes.schema';
 import { PayrollConfigurationModule } from '../payroll-configuration/payroll-configuration.module';
 import { PayrollExecutionModule } from '../payroll-execution/payroll-execution.module';
 
